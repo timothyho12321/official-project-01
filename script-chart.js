cabb96f87b1631164a8ccbb9886c4786979811cb
timothyho12321 <113851311+timothyho12321@users.noreply.github.com>
2022-10-28 01:25:47 +0800

    Added chart for percentage of people who participated in sport

--- script-chart.js
+++ script-chart.js
@@ -168,6 +168,143 @@ async function main() {
         }
 
     ])
+
+
+
+    //LOAD CHART FOR PARTICIPATE IN SPORTS
+
+
+    let chartOptions2 = {
+        chart: {
+            type: 'line',
+            height: '100%'
+
+        },
+
+        series: [
+
+        ],
+
+        noData: {
+            text: "Please wait while data loads"
+        }
+
+    }
+
+
+    const chart2 = new ApexCharts(
+        document.querySelector("#chart-participate"), chartOptions2
+
+
+    )
+
+    chart2.render();
+
+
+
+    async function loadData2() {
+        let response = await axios.get("participate-sports.csv");
+
+        let json = await csv().fromString(response.data);
+        console.log(json);
+        return (json);
+
+
+    }
+
+    //Add data of regular participate
+    let participateSports = await loadData2();
+    let regularParticipate = transformData4(participateSports);
+    function transformData4(rawData) {
+        let shortlisted = [];
+        // console.log(rawData);
+        for (let record of rawData) {
+            shortlisted.push({
+                'x': parseInt(record.annual),
+                'y': parseInt(record.regular)
+
+
+            });
+        }
+        console.log(shortlisted);
+        return shortlisted
+    }
+
+     //Add data of irregular participate
+     let irregularParticipate = transformData5(participateSports);
+     function transformData5(rawData) {
+         let shortlisted = [];
+         // console.log(rawData);
+         for (let record of rawData) {
+             shortlisted.push({
+                 'x': parseInt(record.annual),
+                 'y': parseInt(record.irregular)
+ 
+ 
+             });
+         }
+         console.log(shortlisted);
+         return shortlisted
+     }
+
+     //Add data of sedentary
+     let sedentary = transformData6(participateSports);
+     function transformData6(rawData) {
+         let shortlisted = [];
+         // console.log(rawData);
+         for (let record of rawData) {
+             shortlisted.push({
+                 'x': parseInt(record.annual),
+                 'y': parseInt(record.sedentary)
+ 
+ 
+             });
+         }
+         console.log(shortlisted);
+         return shortlisted
+     }
+
+      //Add data of noparticipate
+      let noparticipate = transformData7(participateSports);
+      function transformData7(rawData) {
+          let shortlisted = [];
+          // console.log(rawData);
+          for (let record of rawData) {
+              shortlisted.push({
+                  'x': parseInt(record.annual),
+                  'y': parseInt(record.didnotparticipatepastyear)
+  
+  
+              });
+          }
+          console.log(shortlisted);
+          return shortlisted
+      }
+ 
+
+
+
+
+    chart2.updateSeries([
+        {
+            name: "Regular",
+            data: regularParticipate
+        },
+        {
+            name: "Irregular",
+            data: irregularParticipate
+        },
+        {
+            name: "Sedentary",
+            data: sedentary
+        },
+        {
+            name: "No participation",
+            data: noparticipate
+        }
+
+    ])
+
 }
 main();
 
